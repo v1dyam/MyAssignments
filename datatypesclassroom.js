05fdd41f270096f471db087a4dd2b13d3d60f7bf
v1dyam <127000644+v1dyam@users.noreply.github.com>
2026-04-24 00:31:15 +0530

    Add files via upload
    week1day1_classroom activity datatypes

---
+++ datatypesclassroom.js
@@ -0,0 +1,17 @@
+    let firstName = "vidyashree";
+    console.log(firstName)
+
+    let companyName="virtusa";
+    console.log(companyName)
+
+    let mobileNumber =987456321;
+    console.log(mobileNumber)
+
+    let myjobisAutomation =true;
+     console.log(myjobisAutomation)
+
+     let hasplaywright;
+    console.log(hasplaywright)
+    
+      companyName="virtusa";
+    console.log(typeof "companyName")
