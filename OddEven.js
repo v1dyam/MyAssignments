ab99ec023be633b7b5edab3c04aa4607843690f6
v1dyam <127000644+v1dyam@users.noreply.github.com>
2026-04-28 22:04:25 +0530

    Add files via upload

---
+++ OddEven.js
@@ -0,0 +1,19 @@
+
+function OddorEven(num)
+
+{
+    if (num%2===0)
+  console.log ("it is an  even number");
+
+    }  {
+        
+        console.log ("it is an  odd number");
+}
+    OddorEven(20);
+    OddorEven(15);
+    
+
+
+
+
+
