6146812b0b5dc3f7c7126c393c74f0f86fa95f2a
v1dyam <127000644+v1dyam@users.noreply.github.com>
2026-04-28 23:44:04 +0530

    Add files via upload

---
+++ numbertype.js
@@ -0,0 +1,27 @@
+function funname(num){
+let a = 0
+  
+    if (num>a){
+        console.log("number is positive");
+    }
+
+    else if(num<a) {
+      console.log("number is negative");
+    }
+
+    else
+        {
+
+        console.log("number is zero")
+    }
+
+}
+     funname(5);
+     funname(-6);
+     funname(0);
+
+
+
+
+
+
